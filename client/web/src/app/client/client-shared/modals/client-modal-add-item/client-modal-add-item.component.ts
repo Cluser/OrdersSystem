b0eee192d4dd0a9140b3e1025f93df90a2ddb817
cluser <mrbfilipiak@gmail.com>
2021-12-30 13:39:31 +0100

    Add select inputs to add item modal

--- client/web/src/app/client/client-shared/modals/client-modal-add-item/client-modal-add-item.component.ts
+++ client/web/src/app/client/client-shared/modals/client-modal-add-item/client-modal-add-item.component.ts
@@ -1,7 +1,6 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { ApiService } from '../../api/api.service';
-import { IItemToOrder } from '../../models/models';
+import { IDistributor, IItemToOrder, IProject } from '../../models/models';
 
 @Component({
   selector: 'app-client-modal-add-item',
@@ -13,16 +12,29 @@ export class ClientModalAddItemComponent implements OnInit {
   @Output() itemAdded: EventEmitter<any> = new EventEmitter();
 
   public item: IItemToOrder = {};
+  public projects: IProject[] = [];
+  public distributors: IDistributor[] = [];
 
   constructor(private api: ApiService) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getProjects();
+    this.getDistributors();
+  }
+
+  public getProjects(): void {
+    this.api.getProjects().subscribe((projects) => this.projects = projects);
+  }
+
+  public getDistributors(): void {
+    this.api.getDistributors().subscribe((distributors) => this.distributors = distributors);
+  }
 
   public addItem(item: IItemToOrder): void {
     this.api.addItemsToOrder(item).subscribe(() => this.close());
   }
 
-  public close() {
+  public close(): void {
     this.itemAdded.emit();
   }
 
