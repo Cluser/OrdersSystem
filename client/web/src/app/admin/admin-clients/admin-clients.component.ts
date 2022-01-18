aecf564c8e8e2f7af669ff16c31f940d2d68cd36
cluser <mrbfilipiak@gmail.com>
2022-01-18 14:19:26 +0100

    Add getting of data to admin panel

--- client/web/src/app/admin/admin-clients/admin-clients.component.ts
+++ client/web/src/app/admin/admin-clients/admin-clients.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ColDef } from 'ag-grid-community';
+import { ApiService } from 'src/app/shared/api/api.service';
 
 @Component({
   selector: 'app-admin-clients',
@@ -12,9 +14,23 @@ export class AdminClientsComponent implements OnInit {
   public rowData: any[] = [];
   public pageSize: number = 1000
 
-  constructor() { }
+  constructor(private api: ApiService, private modalService: NgbModal) { }
 
   ngOnInit(): void {
+    this.getClientsData();
+  }
+
+  public getClientsData(): void {
+    this.api.client.getClients({}, 1, this.pageSize).subscribe((response) => this.rowData = response.items);
+    this.columnDefs = [
+      { checkboxSelection: true, flex: 0.5, headerCheckboxSelection: true },
+      { field: 'id', headerName: 'id', sortable: true, filter: true, resizable: true, flex: 1 },
+      { field: 'name', headerName: 'Nazwa', sortable: true, filter: true, resizable: true, flex: 3 },
+      { field: 'email', headerName: 'E-mail', sortable: true, filter: true, resizable: true, flex: 3 },
+      { field: 'phone', headerName: 'Telefon', sortable: true, filter: true, resizable: true, flex: 3 },
+      { field: 'address', headerName: 'Adres', sortable: true, filter: true, resizable: true, flex: 3 },
+      { field: 'description', headerName: 'Opis', sortable: true, filter: true, resizable: true, flex: 3 },
+    ];
   }
 
 }
--- client/web/src/app/admin/admin-users/admin-users.component.ts
+++ client/web/src/app/admin/admin-users/admin-users.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ColDef } from 'ag-grid-community';
+import { ApiService } from 'src/app/shared/api/api.service';
 
 @Component({
   selector: 'app-admin-users',
@@ -12,9 +14,23 @@ export class AdminUsersComponent implements OnInit {
   public rowData: any[] = [];
   public pageSize: number = 1000
 
-  constructor() { }
+  constructor(private api: ApiService, private modalService: NgbModal) { }
 
   ngOnInit(): void {
+    this.getUsersData();
+  }
+
+  public getUsersData(): void {
+    // this.api.users.getClients({}, 1, this.pageSize).subscribe((response) => this.rowData = response.items);
+    // this.columnDefs = [
+    //   { checkboxSelection: true, flex: 0.5, headerCheckboxSelection: true },
+    //   { field: 'id', headerName: 'id', sortable: true, filter: true, resizable: true, flex: 1 },
+    //   { field: 'name', headerName: 'Nazwa', sortable: true, filter: true, resizable: true, flex: 3 },
+    //   { field: 'email', headerName: 'E-mail', sortable: true, filter: true, resizable: true, flex: 3 },
+    //   { field: 'phone', headerName: 'Telefon', sortable: true, filter: true, resizable: true, flex: 3 },
+    //   { field: 'address', headerName: 'Adres', sortable: true, filter: true, resizable: true, flex: 3 },
+    //   { field: 'description', headerName: 'Opis', sortable: true, filter: true, resizable: true, flex: 3 },
+    // ];
   }
 
 }
