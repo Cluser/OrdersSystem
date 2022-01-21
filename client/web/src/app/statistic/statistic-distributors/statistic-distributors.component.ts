ffec37b77f404a2958e29f848a04f321530acd5e
cluser <mrbfilipiak@gmail.com>
2022-01-21 15:25:09 +0100

    Add chart

--- client/web/src/app/statistic/statistic-distributors/statistic-distributors.component.ts
+++ client/web/src/app/statistic/statistic-distributors/statistic-distributors.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/shared/api/api.service';
+import { IOrder } from 'src/app/shared/models';
 
 @Component({
   selector: 'app-statistic-distributors',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatisticDistributorsComponent implements OnInit {
 
-  constructor() { }
+// public saleData = [
+//     { name: "", value: 105000 },
+//   ];
+
+  public chartData = []
+
+  public orders: IOrder[] = [];
+
+  constructor(private api: ApiService) {}
 
   ngOnInit(): void {
+    this.getOrders();
   }
 
+  // private getOrders() {
+  //   this.api.order.getOrders({}, 1, 1000).subscribe((orders) => {
+  //     console.log(orders)
+  //     orders.items.forEach((order: any) => {
+  //       let price = 0;
+  //       order.items.forEach((item: any) => {
+  //         price = price + item.price;
+  //         console.log(this.saleData)
+  //       })
+  //       this.saleData.push({name: order.distributor.name, value: price} )
+  //       this.saleData = [...this.saleData]
+  //     })
+
+  //   });
+  // }
+
+  private getOrders() {
+    this.api.order.getOrders({}, 1, 1000).subscribe((orders) => {
+      orders.items
+    });
+  }
+  
 }
