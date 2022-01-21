3b94208523577573eb9a82a159e9afd1b46711dd
cluser <mrbfilipiak@gmail.com>
2022-01-21 15:36:55 +0100

    Get order items

--- server/api/actions/ordersItems.py
+++ server/api/actions/ordersItems.py
@@ -13,6 +13,20 @@ from datetime import datetime
 router = APIRouter()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
+@router.get("/OrdersItems", tags=["OrdersItems"])
+async def get(Item_id: Optional[int] = None, order_id: Optional[int] = None, quantity: Optional[int] = None, price: Optional[int] = None, status: Optional[str] = None, page: Optional[int] = 1, size: Optional[int] = 50) -> List[schemas.ItemOrder]:
+    try:
+        parameters = {"Item_id": Item_id, "order_id": order_id, 'quantity': quantity, 'price': price, 'status': status}
+        selectedParameters = {key: value for key, value in parameters.items() if value is not None}
+        filters = [getattr(models.ItemOrder, attribute) == value for attribute, value in selectedParameters.items()]
+        OrdersItems = paginate(Db.session.query(models.ItemOrder).options(joinedload(models.ItemOrder.order)).filter(and_(*filters)), page, size)
+    except:
+        Db.session.rollback()
+        raise
+    finally:
+        Db.session.close()
+        return OrdersItems
+
 @router.post("/OrdersItems", tags=["OrdersItems"])
 async def post(orderItems: List[schemas.OrderItemCreate]):
     try:
