c8c04ae1059d76d90cdba0b9b60cbe7238f51fcd
cluser <mrbfilipiak@gmail.com>
2022-01-23 22:34:00 +0100

    Add new statistics

--- server/api/actions/statistic.py
+++ server/api/actions/statistic.py
@@ -12,6 +12,72 @@ from datetime import datetime
 router = APIRouter()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
+
+@router.get("/StatisticsAllCosts", tags=["Statistics"])
+async def getAllCosts():
+       orderItems = Db.session.query(models.ItemOrder)
+
+       price = 0
+       for orderItem in orderItems:
+              price = price + orderItem.price
+
+       return {'name': 'total', 'value': price}
+
+
+@router.get("/StatisticsAllOrderedItems", tags=["Statistics"])
+async def getCostByDistributor():
+       orderItems = Db.session.query(models.ItemOrder)
+
+       count = 0
+       for orderItem in orderItems:
+              count = count + 1
+
+       return {'name': 'total', 'value': count}
+
+
+
+
+@router.get("/StatisticsByUserReq", tags=["Statistics"])
+async def getCostByDistributor():
+       statistic = []
+       users = Db.session.query(models.User)
+       for user in users:
+              orderItems = Db.session.query(models.ItemOrder).filter(models.ItemOrder.item.has(models.Item.idUser == user.id))
+              price = 0
+              for orderItem in orderItems:
+                     price = price + orderItem.price
+              statistic.append({'name': user.name, 'value': price})
+       return statistic
+
+
+@router.get("/StatisticsByUser", tags=["Statistics"])
+async def getCostByDistributor():
+       statistic = []
+       users = Db.session.query(models.User)
+       for user in users:
+              orderItems = Db.session.query(models.ItemOrder).filter(models.ItemOrder.order.has(models.Order.idUser == user.id))
+              price = 0
+              for orderItem in orderItems:
+                     price = price + orderItem.price
+              statistic.append({'name': user.name, 'value': price})
+       return statistic
+
+
+
+@router.get("/StatisticsByDistributor", tags=["Statistics"])
+async def getCostByDistributor():
+       statistic = []
+       distributors = Db.session.query(models.Distributor)
+       for distributor in distributors:
+              orderItems = Db.session.query(models.ItemOrder).filter(models.ItemOrder.order.has(models.Order.idDistributor == distributor.id))
+              price = 0
+              for orderItem in orderItems:
+                     price = price + orderItem.price
+              statistic.append({'name': distributor.name, 'value': price})
+       return statistic
+
+
+
 @router.get("/Statistics", tags=["Statistics"])
 async def getCostByProject():
        statistic = []
